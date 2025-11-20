d0c232b261b42b2b6a55cfe50c8abf1a1177a245
Srijit0 <srijitsaha03@gmail.com>
2025-11-20 13:02:47 +0530

    fix: use runtime-safe views path (path.join(__dirname,'views'))

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // View engine
 app.set('view engine', 'ejs');
-app.set('views', 'C:\\tinylink\\views');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(expressLayouts);
 app.set('layout', 'layout');
